5827891bb8761d1be326022fb05586b396f7aa58
Šárka Prokopová <prokosar@fit.cvut.cz>
2025-09-27 16:55:10 +0200

    new commit

---
+++ src/Detail.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Detail.css';
+
+function Detail() {
+    return(
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    );
+}
+
+export default Detail;
